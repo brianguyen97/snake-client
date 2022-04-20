d3014ad155296a277a3b16258ee31bbe71c479fe
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-20 17:35:08 -0600

    Replace commands to use constants

--- input.js
+++ input.js
@@ -1,19 +1,49 @@
-// setup interface to handle user input from stdin
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function () {
+// constants
+const {
+  MOVE_UP,
+  MOVE_DOWN,
+  MOVE_LEFT,
+  MOVE_RIGHT,
+  messages,
+} = require("./constants");
+
+// setup interface to handle user input from stdin
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
-  const handleUserInput = function (key) {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  };
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
 
   return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+  if (messages[key]) {
+    connection.write(`Say: ${messages[key]}`);
+  }
+  if (key === "w") {
+    connection.write(MOVE_UP);
+  }
+  if (key === "a") {
+    connection.write(MOVE_LEFT);
+  }
+  if (key === "s") {
+    connection.write(MOVE_DOWN);
+  }
+  if (key === "d") {
+    connection.write(MOVE_RIGHT);
+  }
 };
 
 module.exports = { setupInput };
