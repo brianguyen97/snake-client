fe574ca3e6e0f5f974aa7c15b445a2162a4c47f2
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-20 17:35:44 -0600

    Update client.js to use constants

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
-const net = require("net");
-
 // establishes a connection with the game server
+const net = require("net");
+const { IP, PORT } = require("./constants");
 const connect = function () {
   const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: "50541",
+    host: IP,
+    port: PORT,
   });
 
   // interpret incoming data as text
@@ -33,7 +33,4 @@ const connect = function () {
 
 // Export
 
-module.exports = {
-  connect,
-  net,
-};
+module.exports = { connect };
