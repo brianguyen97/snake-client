d0349a4d0b2a821dc09ea8049ba528818f8c5f78
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-20 17:35:31 -0600

    Create constants.js to store hard coded values

---
+++ constants.js
@@ -0,0 +1,20 @@
+const IP = "localhost";
+const PORT = 50541;
+const MOVE_UP = "Move: up";
+const MOVE_DOWN = "Move: down";
+const MOVE_RIGHT = "Move: right";
+const MOVE_LEFT = "Move: left";
+const messages = {
+  z: "Hello!",
+  x: "Good game!",
+  c: "Good-bye!",
+};
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP,
+  MOVE_DOWN,
+  MOVE_RIGHT,
+  MOVE_LEFT,
+  messages,
+};
