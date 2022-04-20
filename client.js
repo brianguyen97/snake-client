93d0906bfbb98298755ec69c43af7e48d63c5bdf
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-04-20 17:59:09 -0600

    Removed some comments

--- client.js
+++ client.js
@@ -7,7 +7,6 @@ const connect = function () {
     port: PORT,
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("data", (message) => {
@@ -15,15 +14,11 @@ const connect = function () {
   });
 
   conn.on("connect", () => {
-    // message when connect to server
     console.log("Succesfully connected to game server");
-    // name for snake
     conn.write("Name: BN");
   });
 
   return conn;
 };
-
-// Export
 
 module.exports = { connect };
